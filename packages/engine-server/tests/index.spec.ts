c357f6fc91237450b3464f31ebc5decdc0818799
Martyn Janes <martyn@obany.com>
2024-11-19 13:43:48 +0200

    Add json assertion

--- packages/engine-server/tests/index.spec.ts
+++ packages/engine-server/tests/index.spec.ts
@@ -3,7 +3,7 @@
 /* eslint-disable max-classes-per-file */
 import { ComponentFactory, I18n, ObjectHelper, type IComponent } from "@twin.org/core";
 import { EngineCore } from "@twin.org/engine-core";
-import engineLocales from "@twin.org/engine-core/locales/en.json";
+import engineLocales from "@twin.org/engine-core/locales/en.json" assert { type: "json" };
 import {
 	AttestationComponentType,
 	AttestationConnectorType,
